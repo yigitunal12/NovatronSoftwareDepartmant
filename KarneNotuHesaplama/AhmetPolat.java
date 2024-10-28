f932b039f8fb2d2a3427b70dadc27edff9cc8794
XAhmetPX <34apolat@gmail.com>
2024-10-28 18:17:48 +0300

    Update and rename Karnepuanhesabla.java to AhmetPolat.java

--- KarneNotuHesaplama/AhmetPolat.java
+++ KarneNotuHesaplama/AhmetPolat.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Karnepuanhesabla {
+public class AhmetPolat {
     public static void main(String[] args){
         Scanner input = new Scanner (System.in);
         double sonuc;
