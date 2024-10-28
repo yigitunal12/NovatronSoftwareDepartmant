bd0ffbf784fe97bd354a3ee1427cbc2b7d078154
Aerodynamiks <58636001+Aerodynamiks@users.noreply.github.com>
2024-10-28 17:52:36 +0300

    Ödev

---
+++ BirdenOnaKadar/OmerMehmet.java
@@ -0,0 +1,10 @@
+public class OmerMehmet {
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
