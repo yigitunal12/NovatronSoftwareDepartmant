26fb3b1045cdfe65667f08d297f89c8c14570445
YagmurDeniz2004 <denizyagmur570@gmail.com>
2024-11-10 21:52:52 +0300

    Add files via upload

---
+++ if-else/PozitifNegatif/Pozitif.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Pozitif {
+   public static void main(String[] args) {
+Scanner input =new Scanner(System.in);
+int sayi1;
+System.out.println("Sayi giriniz :");
+sayi1= input.nextInt();
+ if(sayi1>=0){
+    System.out.println("Pozitif");
+ }
+ if(sayi1<=0){
+    System.out.println("Negatif");
+ }
+
+ }
+   
+      
+}
